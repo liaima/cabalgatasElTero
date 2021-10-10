8dc38d601d9c5d1823f70010e32c411f50a394be
manuel <liaimasoft@gmail.com>
2021-10-09 21:04:14 -0300

    modal Centrado

--- src/app/views/site/reservas.php
+++ src/app/views/site/reservas.php
@@ -90,7 +90,7 @@ $i = 0;
 
       <!-- Modal -->
       <div class="modal fade" id="exampleModal<?=$i?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLabel">Borrar Reserva</h5>
